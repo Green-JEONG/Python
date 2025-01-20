f7a96972f91556c6e4934ca51ba823620932c617
Green-JEONG <th2gr22n@gmail.com>
2025-01-21 00:01:29 +0900

    Feat: Two of Data Types

--- Python으로_웹_스크래퍼_만들기/main.py
+++ Python으로_웹_스크래퍼_만들기/main.py
@@ -1,12 +1,14 @@
-a = 2
-b = 3
-c = a + b
-c = 1
-b = 10
-print(c)
-
-my_age = 88
-# my age = 88 (불가능)
-myAge = 88
-
+# 숫자
+a = 654
 print(a)
+
+# 데이터 타입
+
+# 1. 문자열
+my_name1 = "green"
+my_name2 = "12" # 따옴표로 감싸면 문자가 됨.
+print(my_name1)
+print(my_name2)
+
+# 2. boolean: 참(ture, on, 1) 또는 거짓(false, off, 0) 상태
+my_name3 = True # 맨 앞은 대문자여야 함
