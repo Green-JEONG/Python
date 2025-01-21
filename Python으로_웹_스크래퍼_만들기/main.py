7758f2aca6fbe909c8b58c979f7ed3c34944f54b
Green-JEONG <th2gr22n@gmail.com>
2025-01-21 09:48:41 +0900

    Recap: Variable and Data Type

--- Python으로_웹_스크래퍼_만들기/main.py
+++ Python으로_웹_스크래퍼_만들기/main.py
@@ -1,14 +1,7 @@
-# 숫자
-a = 654
-print(a)
+# 변수의 데이터 재사용
 
-# 데이터 타입
-
-# 1. 문자열
-my_name1 = "green"
-my_name2 = "12" # 따옴표로 감싸면 문자가 됨.
-print(my_name1)
-print(my_name2)
-
-# 2. boolean: 참(ture, on, 1) 또는 거짓(false, off, 0) 상태
-my_name3 = True # 맨 앞은 대문자여야 함
+my_name = "green"
+age = 12
+dead = False
+print("Hello my name is", my_name)
+print("and I'm ", age, " years old")
