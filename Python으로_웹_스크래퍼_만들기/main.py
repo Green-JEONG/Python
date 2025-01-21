8fe96702f3b2830a1bf61f91f642aeb03094a7da
Green-JEONG <th2gr22n@gmail.com>
2025-01-22 00:26:40 +0900

    Feat: Created Simple Tax Calculator

--- Python으로_웹_스크래퍼_만들기/main.py
+++ Python으로_웹_스크래퍼_만들기/main.py
@@ -1,9 +1,9 @@
-# 함수가 여러 개의 parameter를 받는 법
+# 함수: 한번 작성해서 몇번이고 재사용 가능한 코드
+# parameter: 함수 안으로 데이터를 보내서 함수의 결과를 바꾸는 것, 함수 외부로부터 받게 되는 데이터
+# Python은 공백에 민감
 
-# parameter: 함수로 전달하는 데이터를 저장하기 위한 placeholder(그릇)
-def say_hello(user_name, user_age,):
-  print("hello", user_name)
-  print("you are", user_age, "years old")
+def tax_calculator(money):
+  print(money * 0.35)
 
-# call: 괄호
-say_hello("nico", 12) # argument 순서 중요
+tax_calculator(15000000000000)
+tax_calculator(150)
