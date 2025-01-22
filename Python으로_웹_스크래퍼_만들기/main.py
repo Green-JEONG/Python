ccdaf66bee8126a3b0911a6c173b0f7fd1b9250b
Green-JEONG <th2gr22n@gmail.com>
2025-01-22 14:29:37 +0900

    Feat: Assigned default value on parameter

--- Python으로_웹_스크래퍼_만들기/main.py
+++ Python으로_웹_스크래퍼_만들기/main.py
@@ -1,9 +1,7 @@
-# 함수: 한번 작성해서 몇번이고 재사용 가능한 코드
-# parameter: 함수 안으로 데이터를 보내서 함수의 결과를 바꾸는 것, 함수 외부로부터 받게 되는 데이터
-# Python은 공백에 민감
+# parameter에 기본값 주기
+def say_hello(user_name="anonymous"):
+  print("hello", user_name)
 
-def tax_calculator(money):
-  print(money * 0.35)
-
-tax_calculator(15000000000000)
-tax_calculator(150)
+say_hello("green")
+# say_hello() # 에러
+say_hello()
